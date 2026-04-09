7ba096208111874b78decd0cb5e0c901f58b4b42
rash-ad <Rashad.mohd2004@gmail.com>
2026-04-09 13:06:39 +0530

    add register user in flashkart in temporary array store

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -1,28 +1,24 @@
 let users = []; 
-
+let userIdCounter = 1;
 export async function registerUser(req, res) {
     const { name, email, password } = req.body;
 
-    // 1️⃣ Check all fields
     if (!name || !email || !password) {
         return res.status(400).json({ message: "Please provide all fields" });
     }
 
-    // 2️⃣ Check if user already exists
     const existingUser = users.find(u => u.email === email);
     if (existingUser) {
         return res.status(400).json({ message: "User already exists" });
     }
-
-    // 3️⃣ Add new user to array
-    const newUser = { name, email, password };
+    const newUser = { id: userIdCounter++, name, email, password };
     users.push(newUser);
 
-    // 4️⃣ Send 201 response
     return res.status(201).json({ message: "User registered successfully", user: newUser });
 };
 
 export async function getUser(req, res) {
+  res.status(200).json({ message: "Get  all user endpoint", users });
     res.status(200).json({ message: "Get user endpoint", users });
 };
 
