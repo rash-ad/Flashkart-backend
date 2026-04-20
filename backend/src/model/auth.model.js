c5b127279fd14161df5dc48bf41e9c9b9c01c19a
rash-ad <Rashad.mohd2004@gmail.com>
2026-04-20 19:38:43 +0530

    feat: add JWT authentication middleware and user model for authentication

---
+++ backend/src/model/auth.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+const authSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+export const Auth = mongoose.model("Auth", authSchema);
